04385a06a540f8f81f885b3d4cad3ad67b907882
bryanapellanes <bryan.apellanes@gmail.com>
2015-12-12 09:38:40 -0800

    cleaning up

--- ProjectTemplates/BAMvvm/Scripts/_references.js
+++
@@ -1,6 +0,0 @@
-/// <reference path="jquery-2.0.3.js" />
-/// <reference path="jquery-ui-1.10.3.js" />
-/// <reference path="jquery.validate.js" />
-/// <reference path="jquery.validate.unobtrusive.js" />
-/// <reference path="knockout-2.1.0.debug.js" />
-/// <reference path="modernizr-2.6.2.js" />
