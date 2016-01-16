a8214ff7aec7f11eded7d7560a59a0cb64eae355
bryanapellanes <bryan.apellanes@gmail.com>
2016-01-16 14:00:40 -0800

    just do it

--- BuildScripts/Bam.Net/BuildMachineOnly/create_nuget.cmd
+++ BuildScripts/Bam.Net/BuildMachineOnly/create_nuget.cmd
@@ -1,12 +1,13 @@
-
+rem %1 major, %2 minor, %3 patch
 call clean.cmd
 call set_ver.cmd %1 %2 %3
 call build_solution.cmd Release v4.5 c:\src
 call build_toolkit.cmd
 call set_owners_and_authors.cmd "Bryan Apellanes"
 call set_major_minor_patch.cmd
+call pack.cmd
 
-echo "Ready to build Nuget packages"
+echo "Ready to push Nuget packages"
 echo "use 'show' to see nuspec details"
-echo "call pack"
-echo "call push"
+
+echo "type 'push' when ready"
