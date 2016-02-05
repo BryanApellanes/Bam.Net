d3bfb9a04123c1e4ebfec47ed92ea83b5b4f1d1b
bryanapellanes <bryan.apellanes@gmail.com>
2016-02-04 19:11:49 -0800

    testing

---
+++ git-push-all.cmd
@@ -0,0 +1,11 @@
+c:
+cd \BamContent\apps
+call c:\src\Bam.Net\git-push.cmd
+cd \BamContent\common\bamvvm
+call c:\src\Bam.Net\git-push.cmd
+cd \BamContent
+call c:\src\Bam.Net\git-push.cmd
+cd \src\Business
+call c:\src\Bam.Net\git-push.cmd
+cd \src\Bam.Net
+call c:\src\Bam.Net\git-push.cmd
