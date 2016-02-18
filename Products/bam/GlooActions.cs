0c7b02e648805628bb5e000c222c80a39a14f7e0
bryanapellanes <bryan.apellanes@gmail.com>
2016-02-17 20:22:20 -0800

    fix coreservices

---
+++ Products/bam/GlooActions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bam.Net.Testing;
+
+namespace bam
+{
+    [Serializable]
+    public class GlooActions: CommandLineTestInterface
+    {
+    }
+}
