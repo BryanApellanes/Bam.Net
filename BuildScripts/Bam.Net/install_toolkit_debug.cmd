9f13b325b7838269d097022afff5e0443b5a3cd6
bryanapellanes <bryan.apellanes@gmail.com>
2016-02-20 18:47:28 -0800

    debug build script

--- BuildScripts/Bam.Net/install_toolkit_debug.cmd
+++ BuildScripts/Bam.Net/install_toolkit_debug.cmd
@@ -1,3 +1,3 @@
 del /F /Q %temp%\BamToolkit-debug.msi
-copy %1\Bam.Net\BuildScripts\Bam\BamToolkit-debug\lib\net45\BamToolkit-debug.msi %temp%\BamToolkit-debug.msi
+copy %1\Bam.Net\BuildScripts\Bam.Net\BamToolkit-debug\lib\net45\BamToolkit-debug.msi %temp%\BamToolkit-debug.msi
 msiexec /i %temp%\BamToolkit-debug.msi /qb 
