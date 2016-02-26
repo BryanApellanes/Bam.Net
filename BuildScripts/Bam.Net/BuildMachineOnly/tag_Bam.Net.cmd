6beba82b25b558d79104fd1a64e397f3f807e6fa
bryanapellanes <bryan.apellanes@gmail.com>
2016-02-26 14:20:16 -0800

    build script fix

--- BuildScripts/Bam.Net/BuildMachineOnly/tag_Bam.Net.cmd
+++
@@ -1,2 +0,0 @@
-rem %1 - major, %2 - minor, %3 - patch, %4 - commit message
-call tag.cmd Bam.Net %1 %2 %3 %4
--- BuildScripts/Bam.Net/build_and_run_tests_w_coverage.cmd
+++ BuildScripts/Bam.Net/build_and_run_tests_w_coverage.cmd
@@ -1,2 +1,2 @@
-call build_solution Debug v4.5 %1
+call build_solution Debug %1
 call run_tests_w_coverage.cmd
