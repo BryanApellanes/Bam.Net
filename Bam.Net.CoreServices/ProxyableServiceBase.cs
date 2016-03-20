e44c92f6800243bd5a94d4da04ede58607c13e92
bryanapellanes <bryan.apellanes@gmail.com>
2016-03-20 11:02:27 -0700

    extract proxableservicebase

---
+++ Bam.Net.CoreServices/ProxyableServiceBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bam.Net.Data;
+using Bam.Net.Logging;
+using Bam.Net.ServiceProxy;
+
+namespace Bam.Net.CoreServices
+{
+    public class ProxyableServiceBase: Loggable, IRequiresHttpContext
+    {
+        [Exclude]
+        public IHttpContext HttpContext
+        {
+            get;
+            set;
+        }
+    }
+}
