a72cc5060b8e19a909aed4ade0f79daca8c76551
bryanapellanes <bryan.apellanes@gmail.com>
2016-03-27 12:44:57 -0700

    build script: copy msi to buildoutput

--- BuildScripts/Bam.Net/build_toolkit.cmd
+++ BuildScripts/Bam.Net/build_toolkit.cmd
@@ -1,6 +1,8 @@
 rmdir ..\..\Products\BamToolkit\BamToolkit-cache /S /Q
 rmdir ..\..\Products\BamToolkit\BamToolkit-SetupFiles /S /Q
 "C:\Program Files (x86)\Caphyon\Advanced Installer 12.1\bin\x86\AdvancedInstaller.com" /build ..\..\Products\BamToolkit\BamToolkit.aip
+MD .\BuildOutput\msi\
+copy ..\..\Products\BamToolkit\BamToolkit-SetupFiles\BamToolkit.msi .\BuildOutput\msi\
 MD .\BamToolkit\lib\net45\
 copy ..\..\Products\BamToolkit\BamToolkit-SetupFiles\BamToolkit.msi .\BamToolkit\lib\net45\
 nuget pack BamToolkit\BamToolkit.nuspec
--- BuildScripts/Bam.Net/build_toolkit_debug.cmd
+++ BuildScripts/Bam.Net/build_toolkit_debug.cmd
@@ -1,6 +1,8 @@
 rmdir ..\..\Products\BamToolkit\BamToolkit-debug-cache /S /Q
 rmdir ..\..\Products\BamToolkit\BamToolkit-debug-SetupFiles /S /Q
 "C:\Program Files (x86)\Caphyon\Advanced Installer 12.1\bin\x86\AdvancedInstaller.com" /build ..\..\Products\BamToolkit\BamToolkit-debug.aip
+MD .\BuildOutput\msi-debug\
+copy ..\..\Products\BamToolkit\BamToolkit-SetupFiles\BamToolkit.msi .\BuildOutput\msi-debug\
 MD .\BamToolkit-debug\lib\net45\
 copy ..\..\Products\BamToolkit\BamToolkit-debug-SetupFiles\BamToolkit-debug.msi .\BamToolkit-debug\lib\net45\
 nuget pack BamToolkit-debug\BamToolkit-debug.nuspec
