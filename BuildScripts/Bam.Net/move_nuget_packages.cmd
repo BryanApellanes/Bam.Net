dd16079ec67164014b76221a330c7894d9ba6488
bryanapellanes <bryan.apellanes@gmail.com>
2016-03-28 17:18:47 -0700

    update build scripts

--- BuildScripts/Bam.Net/move_nuget_packages.cmd
+++ BuildScripts/Bam.Net/move_nuget_packages.cmd
@@ -4,3 +4,6 @@ For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 MD "Z:\Workspace\NugetPackages\%mydate%_%mytime%"
 copy *.nupkg "Z:\Workspace\NugetPackages\PUSH
 move /Y *.nupkg "Z:\Workspace\NugetPackages\%mydate%_%mytime%"
+
+MD "Z:\Workspace\Msi\%mydate%_%mytime%"
+copy .\BamToolkit\lib\net45\BamToolkit.msi "Z:\Workspace\Msi\%mydate%_%mytime%"
--- BuildScripts/Bam.Net/move_nuget_packages_debug.cmd
+++ BuildScripts/Bam.Net/move_nuget_packages_debug.cmd
@@ -4,3 +4,6 @@ For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 MD "Z:\Workspace\NugetPackages\%mydate%_%mytime%"
 copy *.nupkg "Z:\Workspace\NugetPackages\Debug
 move /Y *.nupkg "Z:\Workspace\NugetPackages\%mydate%_%mytime%"
+
+MD "Z:\Workspace\Msi-debug\%mydate%_%mytime%"
+copy .\BamToolkit-debug\lib\net45\BamToolkit-debug.msi "Z:\Workspace\Msi-debug\%mydate%_%mytime%"
