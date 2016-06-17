0552d2eef8a66f7cfba0bb0ebe315b6439d42b7d
bryanapellanes <bryan.apellanes@gmail.com>
2016-06-17 10:38:38 -0700

    update startgit.cmd

--- startgit.cmd
+++ startgit.cmd
@@ -5,9 +5,9 @@ cd \src\Bam.Net\BamContent
 start "" "C:\Program Files\Git\usr\bin\mintty.exe" -i "C:\Program Files\Git\mingw64\share\git\git-for-windows.ico" "C:\Program Files\Git\usr\bin\bash.exe" --login -i
 cd \src\Business
 start "" "C:\Program Files\Git\usr\bin\mintty.exe" -i "C:\Program Files\Git\mingw64\share\git\git-for-windows.ico" "C:\Program Files\Git\usr\bin\bash.exe" --login -i
-cd \BamContent
+cd \src\Business\Submodule\Bam.Net
 start "" "C:\Program Files\Git\usr\bin\mintty.exe" -i "C:\Program Files\Git\mingw64\share\git\git-for-windows.ico" "C:\Program Files\Git\usr\bin\bash.exe" --login -i
-cd \BamContent\apps
+cd \BamContent
 start "" "C:\Program Files\Git\usr\bin\mintty.exe" -i "C:\Program Files\Git\mingw64\share\git\git-for-windows.ico" "C:\Program Files\Git\usr\bin\bash.exe" --login -i
 cd \BamContent\common\bamvvm
 start "" "C:\Program Files\Git\usr\bin\mintty.exe" -i "C:\Program Files\Git\mingw64\share\git\git-for-windows.ico" "C:\Program Files\Git\usr\bin\bash.exe" --login -i
