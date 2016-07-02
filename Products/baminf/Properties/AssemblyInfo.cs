c456770e1ae36d08b9ced4f95ca15a4ceee1c3a1
bryanapellanes <bryan.apellanes@gmail.com>
2016-07-01 20:21:40 -0700

    Baminf name correction

--- Products/baminf/Properties/AssemblyInfo.cs
+++ Products/baminf/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("assinf")]
+[assembly: AssemblyTitle("baminf")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("assinf")]
+[assembly: AssemblyProduct("baminf")]
 [assembly: AssemblyCopyright("Copyright © Bryan Apellanes 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
