11cbcd9d360907f789147bf53bec109c4cd5317d
bryanapellanes <bryan.apellanes@gmail.com>
2016-09-14 19:18:49 -0700

    update build script

--- BuildScripts/Bam.Net/build_solution.cmd
+++ BuildScripts/Bam.Net/build_solution.cmd
@@ -9,11 +9,11 @@ call %ROOT%\Bam.Net\Bam.Net.Server\zip_base_app_and_content.cmd %ROOT%
 call .\restore.cmd
 SET OutputPath=%ROOT%\Bam.Net\BuildScripts\Bam.Net\BuildOutput\%CONFIG%\
 
-SET VER=v4.5
-SET NEXT=END
+SET VER=%3
+IF [%3]==[] SET VER=v4.6.2
 
 rmdir /Q /S %OutputPath%%VER%
 mkdir %OutputPath%%VER%
-.\MSBuild\MSBuild.exe /t:Build /filelogger /p:AutoGenerateBindingRedirects=true;GenerateDocumentation=true;OutputPath=%OutputPath%%VER%;Configuration=%CONFIG%;Platform="x64";CompilerVersion=%VER% %2\Bam.Net\Bam.Net.Nuget.sln /m:4
+.\MSBuild\MSBuild.exe /t:Build /filelogger /p:AutoGenerateBindingRedirects=true;GenerateDocumentation=true;OutputPath=%OutputPath%%VER%;Configuration=%CONFIG%;Platform="x64";TargetFrameworkVersion=%VER%;CompilerVersion=%VER% %2\Bam.Net\Bam.Net.Nuget.sln /m:4
 
 EXIT /b %ERRORLEVEL%
