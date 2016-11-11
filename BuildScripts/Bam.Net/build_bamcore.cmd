0b801def2115f63c2b6868f30b7d43751d780cb8
bryanapellanes <bryan.apellanes@gmail.com>
2016-11-10 21:05:40 -0800

    buildscript

--- BuildScripts/Bam.Net/build_bamcore.cmd
+++ BuildScripts/Bam.Net/build_bamcore.cmd
@@ -4,6 +4,6 @@ SET ROOT=%2
 IF [%2]==[] SET ROOT=C:\src
 SET OutputPath=%ROOT%\Bam.Net\BuildScripts\Bam.Net\BuildOutput\%CONFIG%\
 
-xcopy .\BamCoreProject\* .\BuildOutput\%CONFIG%\v4.5\ /S /Y
+xcopy .\BamCoreProject\* .\BuildOutput\%CONFIG%\v4.6.2\ /S /Y
 
-.\MSBuild\MSBuild.exe /t:Build /filelogger /p:OutputPath=%OutputPath%v4.5;Configuration=%CONFIG%;CompilerVersion=v4.5 %OutputPath%\v4.5\BamCore.sln /m:4
+.\MSBuild\MSBuild.exe /t:Build /filelogger /p:OutputPath=%OutputPath%v4.6.2;Configuration=%CONFIG%;CompilerVersion=v4.6.2 %OutputPath%\v4.6.2\BamCore.sln /m:4
