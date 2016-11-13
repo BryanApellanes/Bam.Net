4c112081597d0c168102f1f7df12acd34943ebaf
bryanapellanes <bryan.apellanes@gmail.com>
2016-11-13 10:55:47 -0800

    fix coverage report generation

--- BuildScripts/Bam.Net/generate_coverage_report.cmd
+++ BuildScripts/Bam.Net/generate_coverage_report.cmd
@@ -1,4 +1,4 @@
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 
-".\ReportGenerator\ReportGenerator.exe" -reports:.\BuildOutput\Debug\v4.5\CodeCoverage.xml -targetdir:"Z:\Workspace\TestOutput\CodeCoverageReports\%mydate%_%mytime%" -historydir:"Z:\Workspace\TestOutput\History"
+".\ReportGenerator\ReportGenerator.exe" -reports:.\BuildOutput\Debug\v4.6.2\CodeCoverage.xml -targetdir:"Z:\Workspace\TestOutput\CodeCoverageReports\%mydate%_%mytime%" -historydir:"Z:\Workspace\TestOutput\History"
