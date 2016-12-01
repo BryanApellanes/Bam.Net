e719879f793ba2345f066f78f3c4b15ac1a469c1
bryanapellanes <bryan.apellanes@gmail.com>
2016-11-30 20:48:36 -0800

    Bam.Net.CommandLine: address concurrency issue with console output colors

---
+++ Bam.Net.CommandLine/ConsoleMessage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bam.Net.CommandLine
+{
+    public class ConsoleMessage
+    {
+        public ConsoleMessage(string msg)
+        {
+            Text = msg;
+        }
+
+        public ConsoleMessage(string msg, ConsoleColorCombo colors): this(msg)
+        {
+            Colors = colors;
+        }
+
+        public ConsoleMessage(string msg, ConsoleColor textColor, ConsoleColor background = ConsoleColor.Black) : this(msg, new ConsoleColorCombo(textColor, background))
+        {
+        }
+
+        public string Text { get; set; }
+        public ConsoleColorCombo Colors { get; set; }
+    }
+}
