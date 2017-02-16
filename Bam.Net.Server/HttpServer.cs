1f0999948d988d4582a2aef2e50e2454e679701d
bryanapellanes <bryan.apellanes@gmail.com>
2017-02-16 08:58:32 -0800

    Bam.Net.Server: refactor server for 6x performance improvement

--- Bam.Net.Server/HttpServer.cs
+++ Bam.Net.Server/HttpServer.cs
@@ -9,6 +9,7 @@ using System.Net;
 using System.Threading;
 using Bam.Net;
 using Bam.Net.Logging;
+using System.Threading.Tasks;
 
 namespace Bam.Net.Server
 {
@@ -16,10 +17,6 @@ namespace Bam.Net.Server
     {
         private readonly HttpListener _listener;
         private readonly Thread _handlerThread;
-        private readonly Thread[] _workers;
-        private readonly ManualResetEvent _stop, _ready;
-        private Queue<HttpListenerContext> _queue;
-        private int _port;
         private ILogger _logger;
 
         public HttpServer(ILogger logger = null)
@@ -29,11 +26,7 @@ namespace Bam.Net.Server
         public HttpServer(int maxThreads, ILogger logger)
         {
             logger = logger ?? Log.Default;
-            //_port = port;
-            _workers = new Thread[maxThreads];
-            _queue = new Queue<HttpListenerContext>();
-            _stop = new ManualResetEvent(false);
-            _ready = new ManualResetEvent(false);
+            
             _listener = new HttpListener();
             _handlerThread = new Thread(HandleRequests);
             _logger = logger;
@@ -70,12 +63,6 @@ namespace Bam.Net.Server
             
             _listener.Start();
             _handlerThread.Start();
-
-            for (int i = 0; i < _workers.Length; i++)
-            {
-                _workers[i] = new Thread(Worker);
-                _workers[i].Start();
-            }
         }
 
         public void Dispose()
@@ -88,74 +75,20 @@ namespace Bam.Net.Server
 
         public void Stop()
         {
-            _stop.Set();
-			if (_handlerThread.ThreadState == ThreadState.Running)
+            _listener.Stop();
+            if (_handlerThread.ThreadState == ThreadState.Running)
 			{
 				_handlerThread.Abort();
 				_handlerThread.Join();
 			}
-            foreach (Thread worker in _workers)
-            {
-				if (worker != null)
-				{
-					worker.Join(300);
-				}
-            }
-            _listener.Stop();
         }
-
+        
         private void HandleRequests()
         {
             while (_listener.IsListening)
             {
-                IAsyncResult context = _listener.BeginGetContext(ContextReady, null);
-
-                if (0 == WaitHandle.WaitAny(new[] { _stop, context.AsyncWaitHandle }))
-                    return;
-            }
-        }
-
-        private void ContextReady(IAsyncResult ar)
-        {
-            try
-            {
-                lock (_queue)
-                {
-                    _queue.Enqueue(_listener.EndGetContext(ar));
-                    _ready.Set();
-                }
-            }
-            catch { return; }
-        }
-
-        private void Worker()
-        {
-            WaitHandle[] wait = new[] { _ready, _stop };
-            while (0 == WaitHandle.WaitAny(wait))
-            {
-                HttpListenerContext context;
-                lock (_queue)
-                {
-                    if (_queue.Count > 0)
-                        context = _queue.Dequeue();
-                    else
-                    {
-                        _ready.Reset();
-                        continue;
-                    }
-                }
-
-                try
-                {
-                    if (ProcessRequest != null)
-                    {
-                        ProcessRequest(context);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    _logger.AddEntry("An error occurred processing HTTP request: {0}", ex, ex.Message);
-                }
+                HttpListenerContext context = _listener.GetContext();
+                Task.Run(() => ProcessRequest(context));
             }
         }
 
