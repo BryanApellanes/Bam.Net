6d31c238724146caa02ecb9dfe1f93a2e2dd2332
bryanapellanes <bryan.apellanes@gmail.com>
2017-02-18 13:14:46 -0800

    Bam.Net.Data.Repositories: update docs

--- Bam.Net.Data.Repositories/RepositoryStorableTypesProvider.cs
+++ Bam.Net.Data.Repositories/RepositoryStorableTypesProvider.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace Bam.Net.Data.Repositories
 {
+    /// <summary>
+    /// When implemented by a derived class provides types
+    /// to a repository indicating that those types should
+    /// be supported for persistence operations
+    /// </summary>
     public abstract class RepositoryTypesProvider : IStorableTypesProvider
     {
         public void AddTypes(IRepository repository)
