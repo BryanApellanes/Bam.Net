c69cab7ef8d755385c44feaf1ce37c9588b1dd57
Bryan <bryan@olo.com>
2017-03-01 09:49:04 -0800

    Bam.Net.Services: cleanup, still wip

---
+++ Bam.Net.Services/VersioningService/Data/ClientDescriptor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bam.Net.Data.Repositories;
+
+namespace Bam.Net.Services.VersioningService.Data
+{
+    [Serializable]
+    public class ClientDescriptor: RepoData
+    {
+    }
+}
