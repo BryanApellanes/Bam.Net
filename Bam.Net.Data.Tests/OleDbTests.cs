bc63e42c5f86e770197faf2a6392215502db1b0c
bryanapellanes <bryan.apellanes@gmail.com>
2017-04-30 10:06:09 -0700

    Bam.Net.Data: fix tests to account for changes

--- Bam.Net.Data.Tests/OleDbTests.cs
+++ Bam.Net.Data.Tests/OleDbTests.cs
@@ -12,14 +12,36 @@ namespace Bam.Net.Data.Tests
     [Serializable]
     public class OleDbTests: CommandLineTestInterface
     {
+        [BeforeUnitTests]
+        public void Setup()
+        {
+            FileInfo writeTo = new FileInfo($".\\{nameof(ShouldWriteAccessDatabase)}.accdb");
+            if (File.Exists(writeTo.FullName))
+            {
+                File.Delete(writeTo.FullName);
+            }
+        }
+
+        [AfterUnitTests]
+        public void Teardown()
+        {
+            FileInfo writeTo = new FileInfo($".\\{nameof(ShouldWriteAccessDatabase)}.accdb");
+            if (File.Exists(writeTo.FullName))
+            {
+                File.Delete(writeTo.FullName);
+            }
+        }
+
         [UnitTest]
         public void ShouldWriteAccessDatabase()
         {
+            Setup();
             FileInfo writeTo = new FileInfo($".\\{nameof(ShouldWriteAccessDatabase)}.accdb");
             Expect.IsFalse(File.Exists(writeTo.FullName));
             typeof(OleDbDatabase).Assembly.WriteResource(typeof(OleDbDatabase), "empty.accdb", writeTo);
             Expect.IsTrue(File.Exists(writeTo.FullName));
             OutLine(writeTo.FullName, ConsoleColor.Cyan);
+            Teardown();
         }
     }
 }
