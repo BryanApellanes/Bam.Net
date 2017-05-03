7104ac782c63677145bba127be864894dc9dfc45
bryanapellanes <bryan.apellanes@gmail.com>
2017-05-02 18:12:00 -0700

    Bam.Net: minor bug fixes

--- Bam.Net/AssemblyResolve.cs
+++ Bam.Net/AssemblyResolve.cs
@@ -14,12 +14,17 @@ namespace Bam.Net
         {
             Monitor(loggerProvider());
         }
+        static HashSet<string> _names = new HashSet<string>();
         public static void Monitor(ILogger logger = null)
         {
             logger = logger ?? Log.Default;
             AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
             {
-                logger.AddEntry("Assembly {0}\r\nAt {1}\r\nRequested {2}\r\n\t, but it was not found", LogEventType.Warning, args.RequestingAssembly?.FullName, args.RequestingAssembly?.GetFileInfo().FullName, args.Name);
+                if (!_names.Contains(args.Name))
+                {
+                    _names.Add(args.Name);
+                    logger.AddEntry("Assembly {0}\r\nAt {1}\r\nRequested {2}\r\n\t, but it was not found", LogEventType.Warning, args.RequestingAssembly?.FullName, args.RequestingAssembly?.GetFileInfo().FullName, args.Name);
+                }
                 return null;
             };
         }
