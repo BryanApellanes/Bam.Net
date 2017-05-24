fb2ee63324cf901c96c4ed427acc2039b47bac04
bryanapellanes <bryan.apellanes@gmail.com>
2017-05-23 17:49:00 -0700

    Bam.Net.Logging: add NLog target

--- Bam.Net.Logging/NLog/BamNLogTarget.cs
+++ Bam.Net.Logging/NLog/BamNLogTarget.cs
@@ -8,16 +8,20 @@ using NLog.Targets;
 
 namespace Bam.Net.Logging.NLog
 {
-    public class NLogTarget: Target
+    public class BamNLogTarget: Target
     {
         public static ILogger Logger { get; set; }
         protected override void Write(LogEventInfo logEvent)
         {
-            GetLogActions()[logEvent.Level.Name](logEvent.Message, logEvent.Parameters);
+            GetLogActions()[logEvent.Level.Name]($"{logEvent.LoggerName}:{logEvent.Message}", logEvent.Parameters);
         }   
         
         private Dictionary<string, Action<string, object[]>> GetLogActions()
         {
+            if(Logger == null)
+            {
+                Logger = Log.Default;
+            }
             return new Dictionary<string, Action<string, object[]>>
             {
                 {"Trace", Logger.Info },
@@ -26,7 +30,9 @@ namespace Bam.Net.Logging.NLog
                 {"Warn", Logger.Warning },
                 {"Error", Logger.Error },
                 {"Fatal", Logger.Error },
-                {"Off", (s,o)=>{ } }
+                {"Off", (s,o)=>{ } },
+                {null, (s,o)=>{ } },
+                {"", (s,o)=>{ } }
             };
         }
     }
