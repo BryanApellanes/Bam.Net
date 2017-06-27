159d1872915375c0c95ef72381907a8b0c74780d
bryanapellanes <bryan.apellanes@gmail.com>
2017-06-27 13:49:33 -0700

    Bam.Net.Data: allow/enable select *

---
+++ Bam.Net.Data/ColumnNameListProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bam.Net.Data
+{
+    public delegate string ColumnNameListProvider(Type type, Database db);
+}
