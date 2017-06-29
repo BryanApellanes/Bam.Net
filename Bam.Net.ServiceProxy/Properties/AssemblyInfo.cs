55904e42d9a9345e52f4b8d7d9cb656ec371879e
bryanapellanes <bryan.apellanes@gmail.com>
2017-06-29 15:05:47 -0700

    '1.6.3'

--- Bam.Net.ServiceProxy/Properties/AssemblyInfo.cs
+++ Bam.Net.ServiceProxy/Properties/AssemblyInfo.cs
@@ -45,6 +45,6 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("Bam.Net.ServiceProxy.Tests")]
 [assembly: InternalsVisibleTo("Bam.Net.Services")]
 
-[assembly: AssemblyVersion("1.6.2")]
-[assembly: AssemblyFileVersion("1.6.2")]
+[assembly: AssemblyVersion("1.6.3")]
+[assembly: AssemblyFileVersion("1.6.3")]
 
