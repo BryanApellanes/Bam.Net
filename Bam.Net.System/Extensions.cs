f5c49c23ee7eb9a60c0d4f192442b6a31647a4fe
bryanapellanes <bryan.apellanes@gmail.com>
2017-06-28 20:00:22 -0700

    Bam.Net.System: wmi process

---
+++ Bam.Net.System/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bam.Net.System
+{
+    public static class Extensions
+    {
+        public static void CopyTo(this FileInfo file, string computerName, string remotePath = null)
+        {
+
+        }
+    }
+}
