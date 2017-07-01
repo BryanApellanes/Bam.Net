6d49a8e97fa77b229e00f49a93ea69fda04e42bd
bryanapellanes <bryan.apellanes@gmail.com>
2017-07-01 11:01:23 -0700

    Bam.Net.System: untested copyto

--- Bam.Net.System/Extensions.cs
+++ Bam.Net.System/Extensions.cs
@@ -9,9 +9,32 @@ namespace Bam.Net.System
 {
     public static class Extensions
     {
+        public static void CopyTo(this FileInfo file, string computerName, string userName, string password, string remotePath = null)
+        {
+            using (RunAsContext runasContext = RunAs.Impersonate(userName, password))
+            {
+                CopyTo(file, computerName, remotePath);
+            }
+        }
+
         public static void CopyTo(this FileInfo file, string computerName, string remotePath = null)
         {
-
+            try
+            {
+                remotePath = remotePath ?? "C$\\Windows\\Temp";
+                string destinationFile = Path.Combine(remotePath, file.Name);
+                if(destinationFile.Length >= 2 && destinationFile[1].Equals(":"))
+                {
+                    StringBuilder df = new StringBuilder(destinationFile);
+                    df[1] = '$';
+                    destinationFile = df.ToString();
+                }
+                file.CopyTo($"\\\\{computerName}\\{destinationFile}");
+            }
+            catch (Exception ex)
+            {
+                Logging.Log.Error("Exception copying file ({0}) to target computer ({1}), Path={2}", ex, file.FullName, computerName, remotePath);
+            }
         }
     }
 }
