f63a7fd2646634607bf5b786de17a69b7e1db9b7
bryanapellanes <bryan.apellanes@gmail.com>
2017-07-01 10:23:49 -0700

    Bam.Net.Services: add vault service

--- Bam.Net.Services/Vault/IVaultService.cs
+++ Bam.Net.Services/Vault/IVaultService.cs
@@ -4,10 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Bam.Net.Services.Vault
+namespace Bam.Net.Services
 {
     public interface IVaultService
     {
-        
+        Dictionary<string, string> ExportValues();
+        Dictionary<string, string> ExportUserValues();
+        void ImportValues(Dictionary<string, string> values);
+        void ImportUserValues(Dictionary<string, string> values);
+        string GetValue(string keyName);
+        void SetValue(string keyName, string value);
+        string GetUserValue(string keyName);
+        void SetUserValue(string keyName, string value);
     }
 }
