9b68945c1ba3a73faa4243b0687ed0b0c59b75e9
bryanapellanes <bryan.apellanes@gmail.com>
2017-07-14 20:59:03 -0700

    Bam.Net.Server: HttpServer enable graceful handling of listener collisions

--- Bam.Net.Server/HttpServer.cs
+++ Bam.Net.Server/HttpServer.cs
@@ -10,11 +10,13 @@ using System.Threading;
 using Bam.Net;
 using Bam.Net.Logging;
 using System.Threading.Tasks;
+using System.Collections.Concurrent;
 
 namespace Bam.Net.Server
 {
     public class HttpServer : IDisposable
     {
+        private static ConcurrentDictionary<HostPrefix, HttpServer> _listening = new ConcurrentDictionary<HostPrefix, HttpServer>();
         private readonly HttpListener _listener;
         private readonly Thread _handlerThread;
         private ILogger _logger;
@@ -46,23 +48,62 @@ namespace Bam.Net.Server
             }
         }
         
+        /// <summary>
+        /// If true will attempt to stop
+        /// other HttpServers that are listening on 
+        /// the same port and hostname as what
+        /// the current HttpServer is configured for
+        /// when it is started
+        /// </summary>
+        public bool Usurped
+        {
+            get;
+            set;
+        }
 
         public void Start()
         {
             Start(HostPrefixes);
         }
-        
-        public void Start(params HostPrefix[] hostName)
+        public void Start(params HostPrefix[] hostPrefixes)
         {
-            hostName.Each(hp =>
+            Start(Usurped, hostPrefixes);
+        }
+
+        static object _startLock = new object();
+        public void Start(bool usurped, params HostPrefix[] hostPrefixes)
+        {
+            lock (_startLock)
             {
-                string path = hp.ToString();
-                _logger.AddEntry("HttpServer: {0}", path);
-                _listener.Prefixes.Add(path);
-            });
-            
-            _listener.Start();
-            _handlerThread.Start();
+                hostPrefixes.Each(hp =>
+                {
+                    if (!_listening.ContainsKey(hp))
+                    {
+                        AddHostPrefix(hp);
+                    }
+                    else if (usurped && _listening.ContainsKey(hp))
+                    {
+                        _listening[hp].Stop();
+                        _listening.TryRemove(hp, out HttpServer ignore);
+                        AddHostPrefix(hp);
+                    }
+                    else
+                    {
+                        _logger.AddEntry("HttpServer: Another HttpServer is already listening for host {0}", LogEventType.Warning, hp.ToString());
+                    }
+                });
+
+                _listener.Start();
+                _handlerThread.Start();
+            }
+        }
+
+        private void AddHostPrefix(HostPrefix hp)
+        {
+            _listening.TryAdd(hp, this);
+            string path = hp.ToString();
+            _logger.AddEntry("HttpServer: {0}", path);
+            _listener.Prefixes.Add(path);
         }
 
         public void Dispose()
@@ -72,10 +113,25 @@ namespace Bam.Net.Server
         }
 
 		public bool IsDisposed { get; private set; }
-
+        
         public void Stop()
         {
             _listener.Stop();
+
+            foreach (HostPrefix hp in HostPrefixes)
+            {
+                if (_listening.ContainsKey(hp))
+                {
+                    if(_listening[hp] == this)
+                    {
+                        if (_listening.TryRemove(hp, out HttpServer server))
+                        {
+                            server.Stop();
+                        }
+                    }
+                }
+            }
+            
             if (_handlerThread.ThreadState == ThreadState.Running)
 			{
 				_handlerThread.Abort();
