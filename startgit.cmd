450c443f1500032d734229dfc1dee17570638bee
bryanapellanes <bryan.apellanes@gmail.com>
2017-07-30 19:08:35 -0700

    update startgit.cmd

--- startgit.cmd
+++ startgit.cmd
@@ -1,13 +1,9 @@
 c:
 cd \src\Bam.Net
 "C:\Program Files\Git\git-bash.exe"
-cd \src\Bam.Net\BamContent
-"C:\Program Files\Git\git-bash.exe"
 cd \src\Business
 "C:\Program Files\Git\git-bash.exe"
 cd \src\Business\Submodule\Bam.Net
 "C:\Program Files\Git\git-bash.exe"
 cd \BamContent
-"C:\Program Files\Git\git-bash.exe"
-cd \BamContent\common\bamvvm
 "C:\Program Files\Git\git-bash.exe"
