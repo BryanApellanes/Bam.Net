ee7a3d20a05573c31a752eba0d6eafff10a6442c
bryanapellanes <bryan.apellanes@gmail.com>
2017-10-30 19:54:11 -0700

    add missing file

---
+++ Bam.Net.Services.Clients/CoreFileClient.cs
@@ -0,0 +1,30 @@
+﻿using Bam.Net.CoreServices;
+using Bam.Net.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bam.Net.Services.Clients
+{
+    public class CoreFileClient
+    {
+        CoreFileService _fileService;
+        ProxyFactory _proxyFactory;
+        public CoreFileClient(string hostName, int port, ILogger logger = null)
+        {
+            _proxyFactory = new ProxyFactory(logger);
+            _fileService = _proxyFactory.GetProxy<CoreFileService>(hostName, port);
+        }
+
+        // Resolver.AssemblyResolver
+        //
+        // resolve assembly passes the assembly.fullname
+        // use that to ask the AssemblyService.AssemblyManagementRepository for the 
+        // AssemblyDescriptor by fullname
+        // then AssemblyService.ResolveAssembly by assemblyDescriptor.Name
+        // read the assembly bytes and send them in response Resolver.AssemblyResolver
+
+    }
+}
