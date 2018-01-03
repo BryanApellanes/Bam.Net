ed4095b497bedae75fd2f227079a50b7b915e263
Bryan Apellanes <bryan@threeheadz.com>
2018-01-02 19:31:12 -0800

    Bam.Net.Automation: regen dao to add Top with order by

--- Bam.Net.Automation/ContinuousIntegration/continuousIntegration_Generated/BuildJob.cs
+++ Bam.Net.Automation/ContinuousIntegration/continuousIntegration_Generated/BuildJob.cs
@@ -665,6 +665,25 @@ namespace Bam.Net.Automation.ContinuousIntegration.Data
 			return results;
 		}
 
+		[Bam.Net.Exclude]
+		public static BuildJobCollection Top(int count, QueryFilter where, string orderBy = null, SortOrder sortOrder = SortOrder.Ascending, Database database = null)
+		{
+			Database db = database ?? Db.For<BuildJob>();
+			QuerySet query = GetQuerySet(db);
+			query.Top<BuildJob>(count);
+			query.Where(where);
+
+			if(orderBy != null)
+			{
+				query.OrderBy(orderBy, sortOrder);
+			}
+
+			query.Execute(db);
+			var results = query.Results.As<BuildJobCollection>(0);
+			results.Database = db;
+			return results;
+		}
+
 		/// <summary>
 		/// Execute a query and return the specified number of values.  This method
 		/// will issue a sql TOP clause so only the specified number of values
