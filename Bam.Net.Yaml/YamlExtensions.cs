57cf3415493a23e7a9a7c6555cc280475757cce7
Bryan Apellanes <v-brapel@gmail.com>
2018-01-18 13:59:58 -0800

    Bam.Net.Yaml: added extensions

---
+++ Bam.Net.Yaml/YamlExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Yaml;
+using System.Reflection;
+
+namespace Bam.Net.Yaml
+{
+    public static class YamlExtensions
+    {
+        public static T GetValue<T>(this YamlMapping mapping, string key)
+        {
+            object value = GetValue(mapping, key);
+            if(value is T typed)
+            {
+                return typed;
+            }
+            if (value is YamlMapping childMapping)
+            {
+                return Convert<T>(childMapping);
+            }
+            return default(T);
+        }
+
+        public static object GetValue(this YamlMapping mapping, string key)
+        {
+            object value = mapping[key];
+            if(value is YamlScalar scalar)
+            {
+                return scalar.Value;
+            }
+            return value;
+        }   
+        public static T Convert<T>(this YamlMapping mapping)
+        {
+            return (T)Convert(mapping, typeof(T));
+        }
+
+        public static object Convert(this YamlMapping mapping, Type type) 
+        {
+            object result = type.Construct();
+            foreach (PropertyInfo property in type.GetProperties())
+            {
+                if (mapping.ContainsKey(property.Name))
+                {
+                    YamlNode node = mapping[property.Name];
+                    if (node is YamlScalar value)
+                    {
+                        result.Property(property.Name, System.Convert.ChangeType(value.Value, property.PropertyType));
+                    }
+                    else if (node is YamlMapping childMapping)
+                    {
+                        result.Property(property.Name, childMapping.Convert(property.PropertyType));
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
