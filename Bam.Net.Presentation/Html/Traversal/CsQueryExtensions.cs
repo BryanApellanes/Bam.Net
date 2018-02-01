a71f1136f2db0f3395dab5e7a1f5299545b903cd
Bryan Apellanes <bryan@threeheadz.com>
2018-01-31 17:34:37 -0800

    remove file
    add file

---
+++ Bam.Net.Presentation/Html/Traversal/CsQueryExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsQuery;
+
+namespace Bam.Net.Presentation.Html.Traversal
+{
+    public static class CsQueryExtensions
+    {
+        public static IDomElement NextSiblingElementOfType(this IDomObject obj, string elementType)
+        {
+            IDomElement result = obj.NextElementSibling;
+            while(result != null)
+            {
+                if(result.NodeName.Equals(elementType, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return result;
+                }
+                result = result.NextElementSibling;
+            }
+            return result;
+        }
+
+        public static IEnumerable<IDomElement> EachSiblingUntil(this IDomObject obj, string elementType)
+        {
+            IDomElement next = obj.NextElementSibling;
+            while(next != null)
+            {
+                if(next.NodeName.Equals(elementType, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    break;
+                }
+                yield return next;
+                next = next.NextElementSibling;
+            }
+        }
+
+        public static IDomElement NextSiblingWhere(this IDomObject obj, Predicate<IDomObject> predicate)
+        {
+            IDomElement next = obj.NextElementSibling;
+            while(next != null)
+            {
+                if (predicate(next))
+                {
+                    return next;
+                }
+                next = next.NextElementSibling;
+            }
+            return null;
+        }
+    }
+}
