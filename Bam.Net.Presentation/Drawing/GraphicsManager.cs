6fc1cd97e0e037f0b1361c5452dd98096c8a63ac
Bryan I. Apellanes <bryan@threeheadz.com>
2018-04-15 13:15:34 -0700

    using

--- Bam.Net.Presentation/Drawing/GraphicsManager.cs
+++ Bam.Net.Presentation/Drawing/GraphicsManager.cs
@@ -56,16 +56,18 @@ namespace Bam.Net.Presentation.Drawing
 
             float scale = Math.Min(width / bitmap.Width, height / bitmap.Height);
             Bitmap result = new Bitmap((int)width, (int)height);
-            Graphics graphics = Graphics.FromImage(result);
-            graphics.InterpolationMode = InterpolationMode.High;
-            graphics.CompositingQuality = CompositingQuality.HighQuality;
-            graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            using (Graphics graphics = Graphics.FromImage(result))
+            {
+                graphics.InterpolationMode = InterpolationMode.High;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.SmoothingMode = SmoothingMode.AntiAlias;
 
-            int scaleWidth = (int)(bitmap.Width * scale);
-            int scaleHeight = (int)(bitmap.Height * scale);
-            graphics.FillRectangle(new SolidBrush(backgroundColor.Value), new RectangleF(0, 0, width, height));
-            graphics.DrawImage(bitmap, (width - scaleWidth) / 2, (height - scaleHeight) / 2, scaleWidth, scaleHeight);
-            return result;
+                int scaleWidth = (int)(bitmap.Width * scale);
+                int scaleHeight = (int)(bitmap.Height * scale);
+                graphics.FillRectangle(new SolidBrush(backgroundColor.Value), new RectangleF(0, 0, width, height));
+                graphics.DrawImage(bitmap, (width - scaleWidth) / 2, (height - scaleHeight) / 2, scaleWidth, scaleHeight);
+                return result;
+            }
         }
 
         public Bitmap ResizeCanvas(Bitmap bitmap, int width, int height, Color? backgroundColor = null)
