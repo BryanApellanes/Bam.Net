4dae2d02813cb7d609b475786e70084c9d242d10
Bryan I. Apellanes <bryan@threeheadz.com>
2018-05-04 15:46:14 -0700

    minor syntax refactor

--- Bam.Net.CoreServices/ServiceConfig.cs
+++ Bam.Net.CoreServices/ServiceConfig.cs
@@ -45,8 +45,7 @@ namespace Bam.Net.Server
             string dialect = DefaultConfiguration.GetAppSetting("SqlDialect");
             string connectionString = DefaultConfiguration.GetAppSetting("SqlConnectionString");
 
-            SqlDialect sqlDialect;
-            if(!Enum.TryParse<SqlDialect>(dialect, out sqlDialect))
+            if (!Enum.TryParse<SqlDialect>(dialect, out SqlDialect sqlDialect))
             {
                 sqlDialect = SqlDialect.SQLite;
                 connectionString = "Default";
