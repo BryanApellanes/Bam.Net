5e8f86c8fd5c7d99f12ae0a6014bb92944efff75
Bryan Apellanes <bryan@threeheadz.com>
2018-07-21 11:01:29 -0700

    bam admin; wip

---
+++ Products/bam/BuildClient.cs
@@ -0,0 +1,32 @@
+﻿using Bam.Net.CommandLine;
+using Bam.Net.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bam.Net.Application
+{
+    [Serializable]
+    public class BuildClient : CommandLineTestInterface
+    {
+        [ConsoleAction]
+        public void SetHost()
+        {
+
+        }
+
+        [ConsoleAction]
+        public void GetConfig()
+        {
+
+        }
+
+        [ConsoleAction]
+        public void SetConfig()
+        {
+
+        }
+    }
+}
---
+++ Products/bam/BuildClientInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bam.Net.Application
+{
+    [Serializable]
+    public class BuildClientInfo
+    {
+        public string CoreHostName { get; set; }
+        public string CorePort { get; set; }
+        public string ContentRoot { get; set; }
+    }
+}
