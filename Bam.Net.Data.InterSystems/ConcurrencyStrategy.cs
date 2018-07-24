f963682fa5d66fe7a4cc1d967645cd8b1423125e
Bryan Apellanes <bryan.apellanes@gmail.com>
2018-07-24 16:15:23 -0700

    Bam.Net.UserAccounts.DirectoryServices: implement rolereader methods

---
+++ Bam.Net.Data.InterSystems/ConcurrencyStrategy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bam.Net.Data.InterSystems
+{
+    public enum ConcurrencyStrategy
+    {
+        Invalid = 0,
+        None = 1,
+        Check = 2, // check if anyone has an exclusive lock
+        Read = 3,
+        Write = 4 // 
+    }
+}
