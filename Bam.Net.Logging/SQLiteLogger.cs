650133c135bf6969ffa324251a82a8ccd07ca575
Bryan Apellanes <bryan.apellanes@gmail.com>
2018-08-15 10:14:57 -0700

    Bam.Net.Logger: add parameterless ctor to SQLiteLogger so it can be set as default in config file

--- Bam.Net.Logging/SQLiteLogger.cs
+++ Bam.Net.Logging/SQLiteLogger.cs
@@ -1,4 +1,5 @@
-﻿using Bam.Net.Data.SQLite;
+﻿using Bam.Net.Configuration;
+using Bam.Net.Data.SQLite;
 using Bam.Net.Logging;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,9 @@ namespace Bam.Net.Logging
 {
     public class SQLiteLogger : DaoLogger
     {
+        public SQLiteLogger() : this(new SQLiteDatabase(RuntimeSettings.AppDataFolder, $"{DefaultConfiguration.GetAppSetting("ApplicationName", StaticApplicationNameProvider.DefaultApplicationName)}_Logs"))
+        { }
+
         public SQLiteLogger(SQLiteDatabase database):base(database)
         {
         }
