3bb967fa75f861b2c293e95258add160f99d57b3
Bryan Apellanes <bryan@threeheadz.com>
2018-09-09 13:50:49 -0700

    '1.15.1'

--- Naizari/Properties/AssemblyInfo.cs
+++ Naizari/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("1.15.0")]
-[assembly: AssemblyFileVersion("1.15.0")]
+[assembly: AssemblyVersion("1.15.1")]
+[assembly: AssemblyFileVersion("1.15.1")]
 
