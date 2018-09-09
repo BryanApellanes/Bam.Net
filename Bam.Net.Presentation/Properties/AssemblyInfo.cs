f3249033b65cc62ce64880043d1a5983201f145a
Bryan Apellanes <bryan@threeheadz.com>
2018-09-09 14:02:36 -0700

    '1.16.0'

--- Bam.Net.Presentation/Properties/AssemblyInfo.cs
+++ Bam.Net.Presentation/Properties/AssemblyInfo.cs
@@ -37,5 +37,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: InternalsVisibleTo("Bam.Net.Presentation.Tests")]
 [assembly: InternalsVisibleTo("Bam.Net.Rendering")]
-[assembly: AssemblyVersion("1.15.1")]
-[assembly: AssemblyFileVersion("1.15.1")]
+[assembly: AssemblyVersion("1.16.0")]
+[assembly: AssemblyFileVersion("1.16.0")]
--- Products/Tvg/vyoo/Properties/AssemblyInfo.cs
+++ Products/Tvg/vyoo/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("1.15.1")]
-[assembly: AssemblyFileVersion("1.15.1")]
+[assembly: AssemblyVersion("1.16.0")]
+[assembly: AssemblyFileVersion("1.16.0")]
