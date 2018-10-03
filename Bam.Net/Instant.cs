a11d17924f8fce6442bced3177990ac35da7dae7
Bryan Apellanes <bryan.apellanes@gmail.com>
2018-10-03 07:17:59 -0700

    use $ string concat

--- Bam.Net/Instant.cs
+++ Bam.Net/Instant.cs
@@ -231,7 +231,7 @@ namespace Bam.Net
         /// <returns></returns>
         public string ToDateString()
         {
-            return "{Month}/{Day}/{Year}".NamedFormat(this);
+            return $"{Month}/{Day}/{Year}";
         }
 
         /// <summary>
@@ -240,7 +240,7 @@ namespace Bam.Net
         /// <returns></returns>
         public DateTime ToDateTime(DateTimeKind kind = DateTimeKind.Unspecified)
         {
-            return new DateTime(this.Year, this.Month, this.Day, this.Hour, this.Minute, this.Second, this.Millisecond, DateTimeKind.Unspecified);
+            return new DateTime(this.Year, this.Month, this.Day, this.Hour, this.Minute, this.Second, this.Millisecond, kind);
         }
 
         public DateTime ToLocalTime()
@@ -250,7 +250,7 @@ namespace Bam.Net
 
         public string ToJavascriptDate()
         {
-            return "new Date({Year}, {Month}, {Day}, {Hour}, {Minute}, {Second}, {Millisecond});".NamedFormat(this);
+            return $"new Date({Year}, {Month}, {Day}, {Hour}, {Minute}, {Second}, {Millisecond});";
         }
 
         public int Month
