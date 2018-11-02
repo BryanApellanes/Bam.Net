b65d5cb0ddfa895763b257955b10dfa8790a1cec
Bryan Apellanes <bryan.apellanes@gmail.com>
2018-11-02 10:31:58 -0700

    retarget full framework for bam.exe

--- netCore/bam/Program.cs
+++ netCore/bam/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Bam.Net
 {
@@ -6,7 +10,6 @@ namespace Bam.Net
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
