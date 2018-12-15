dc5ddb84c99dad12e9623bb2cb5e9620d9d383a6
Bryan Apellanes <bryan@threeheadz.com>
2018-12-15 10:15:05 -0800

    restructure root of repo; moved net core up and legacy down

--- Bam.Net/Properties/AssemblyInfo.cs
+++ Bam.Net/Properties/AssemblyInfo.cs
@@ -1,45 +1,43 @@
-/*
-	Copyright © Bryan Apellanes 2015  
-*/
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bam.Net")]
-[assembly: AssemblyDescription("Productivity Tools")]
+[assembly: AssemblyTitle("bam.net.fx")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Bryan Apellanes")]
-[assembly: AssemblyProduct("Bam.Net")]
-[assembly: AssemblyCopyright("Copyright © Bryan Apellanes 2015")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("bam.net.fx")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("369ac7fd-cb1e-4c46-a47e-e84c5037439d")]
+[assembly: Guid("8ac4e2b4-0f3b-4a65-a097-5816ef6ea263")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: InternalsVisibleTo("Bam.Net.Tests")]
-[assembly: InternalsVisibleTo("Bam.Net.Automation.Tests")]
-[assembly: InternalsVisibleTo("Bam.Net.Logging")]
-[assembly: InternalsVisibleTo("Bam.Net.Data.Schema")]
-[assembly: InternalsVisibleTo("Bam.Net.CoreServices")]
-
 [assembly: AssemblyVersion("1.16.1")]
 [assembly: AssemblyFileVersion("1.16.1")]
+
+[assembly: InternalsVisibleTo("bam.net.data.fx.tests")]
+[assembly: InternalsVisibleTo("bam.net.data.repositories.fx.tests")]
+[assembly: InternalsVisibleTo("bam.net.coreservices.fx.tests")]
+[assembly: InternalsVisibleTo("bam.net.presentation.fx.tests")]
+[assembly: InternalsVisibleTo("bam.net.services.fx.tests")]
+[assembly: InternalsVisibleTo("bake")]
