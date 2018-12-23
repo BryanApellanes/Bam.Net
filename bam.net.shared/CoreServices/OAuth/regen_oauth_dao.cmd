dc4bcb131a73553332471a815c46a51e1b5e47a4
Bryan Apellanes <bryan@threeheadz.com>
2018-12-22 16:56:34 -0800

    minor

--- bam.net.shared/CoreServices/OAuth/regen_oauth_dao.cmd
+++ bam.net.shared/CoreServices/OAuth/regen_oauth_dao.cmd
@@ -1,4 +1,3 @@
 ﻿
 
-rem - C:\bam\src\Business\Submodule\Bam.Net\netCore\bam.net.shared\CoreServices\OAuth\Data\Generated_Dao
 troo.exe /gsr /ta:.\bam.net.dll /sn:OAuthSettings /fns:Bam.Net.CoreServices.OAuth.Data /cfi:yes /uis:no /ws:C:\bam\src\generated\Bam.Net.CoreServices\OAuth\Data\Generated_Dao
--- bam.net.shared/CoreServices/WebHooks/regen_webhooks_dao.cmd
+++ bam.net.shared/CoreServices/WebHooks/regen_webhooks_dao.cmd
@@ -1,3 +1,3 @@
 ﻿
 
-troo.exe /gsr /ta:.\bam.net.fx.dll /sn:WebHooks /fns:Bam.Net.CoreServices.WebHooks.Data /cfi:yes /uis:no /ws:C:\bam\src\generated\Bam.Net.CoreServices\WebHooks\Data\Generated_Dao
+troo.exe /gsr /ta:.\bam.net.dll /sn:WebHooks /fns:Bam.Net.CoreServices.WebHooks.Data /cfi:yes /uis:no /ws:C:\bam\src\generated\Bam.Net.CoreServices\WebHooks\Data\Generated_Dao
