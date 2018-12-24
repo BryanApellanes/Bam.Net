09ee7f5d96ced7b64fac88349392bda461d6513a
Bryan Apellanes <bryan@threeheadz.com>
2018-12-24 11:58:17 -0800

    rename spec attribute

--- bam.net.shared/Testing/Specification/SpecTestAttribute.cs
+++ bam.net.shared/Testing/Specification/SpecTestAttribute.cs
@@ -1,13 +1,19 @@
-﻿using Bam.Net.CommandLine;
+/*
+	Copyright © Bryan Apellanes 2015  
+*/
+using Bam.Net.CommandLine;
 using Bam.Net.Testing.Unit;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Bam.Net.Testing.Specification
 {
-    [AttributeUsage(AttributeTargets.Method)]
-    public class SpecTestAttribute: ConsoleActionAttribute
+
+	[AttributeUsage(AttributeTargets.Method)]
+	public class SpecAttribute: ConsoleActionAttribute
     {
-    }
+	}
 }
